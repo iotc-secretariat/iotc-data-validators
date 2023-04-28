d1e40ca25ca0c6a54b3dd238d06db2f2cb4779c4
Fabio Fiorellato <flowerside@gmail.com>
2023-04-28 14:40:27 +0400

    Finalizing current version

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,7 +1,4 @@
 docker build --build-arg BB_user=$BITBUCKET_USER \
 			 --build-arg BB_password=$BITBUCKET_PASSWORD \
-			 --build-arg DB_server=$DEFAULT_IOTC_LIBS_DB_SERVER \
-			 --build-arg DB_user=$DEFAULT_IOTC_LIBS_DB_USER \
-			 --build-arg DB_password=$DEFAULT_IOTC_LIBS_DB_PASSWORD \
 			 -t iotc/validator:latest \
 			 -f Dockerfile .
