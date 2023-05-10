3af7f51ed0a531d8b38e9638d742d730e6a99298
Fabio Fiorellato <ffiorellato@iotc.local>
2023-05-10 14:06:20 +0400

    Adding 4-SF

---
+++ app/4SF/app.R
@@ -0,0 +1,4 @@
+source("../app_common.R")
+
+shinyApp(ui     = function()                       { return(common_ui    ("Form 4-SF", "IOTCForm4SF")) }, 
+         server = function(input, output, session) { return(common_server("Form 4-SF", "IOTCForm4SF", input, output, session)) })
