bdf84d73c46506be3ea5c2f51fd0a6daf616440a
flowerside <flowerside@gmail.com>
2023-05-15 12:48:48 +0400

    Adding 3-CE-multiple

---
+++ app/3CE-multiple/app.R
@@ -0,0 +1,4 @@
+source("../app_common.R")
+
+shinyApp(ui     = function()                       { return(common_ui    ("Form 3-CE-multiple", "IOTCForm3CEMultiple")) }, 
+         server = function(input, output, session) { return(common_server("Form 3-CE-multiple", "IOTCForm3CEMultiple", input, output, session)) })
