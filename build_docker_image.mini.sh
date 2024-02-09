40615995b5473c300b64bf8d8d138da16877b2d5
flowerside <flowerside@gmail.com>
2024-02-09 15:06:42 +0400

    Preparing for 0.1.0 release

--- build_docker_image.mini.sh
+++ build_docker_image.mini.sh
@@ -1,4 +1,4 @@
 docker build --build-arg BB_user=$BITBUCKET_USER \
 			 --build-arg BB_password=$BITBUCKET_PASSWORD \
-			 -t iotc/validator-ui-mini:interim \
+			 -t iotc/validator-ui-mini:0.1.0 \
 			 -f Dockerfile.mini .
--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,4 +1,4 @@
 docker build --build-arg BB_user=$BITBUCKET_USER \
 			 --build-arg BB_password=$BITBUCKET_PASSWORD \
-			 -t iotc/validator-ui:interim \
+			 -t iotc/validator-ui:0.1.0 \
 			 -f Dockerfile .
