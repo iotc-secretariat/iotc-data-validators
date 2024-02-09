bd0491880a0048c336135c5e58c1efdb2a3d6ea5
flowerside <flowerside@gmail.com>
2024-02-09 15:16:23 +0400

    Changing Docker image version

--- build_docker_image.bat
+++ build_docker_image.bat
@@ -1,1 +1,1 @@
-docker build --build-arg BB_user=%BITBUCKET_USER% --build-arg BB_password=%BITBUCKET_PASSWORD% -t iotc/validator-ui:interim -f Dockerfile --progress=plain . 
+docker build --build-arg BB_user=%BITBUCKET_USER% --build-arg BB_password=%BITBUCKET_PASSWORD% -t iotc/validator-ui:0.1.0-interim -f Dockerfile --progress=plain .
