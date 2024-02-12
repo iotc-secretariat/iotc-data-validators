a881819d4370c9e4cb424e5ab5e7e4fbe33249e1
flowerside <flowerside@gmail.com>
2024-02-12 09:58:55 +0400

    Removing .mini Docker config etc.

--- app/update_IOTC_deps.sh
+++
@@ -1,5 +0,0 @@
-echo #################################################################################################################
-echo # Remember to set the BITBUCKET_USER and BITBUCKET_PASSWORD environmental variables before running this script! #
-echo #################################################################################################################
-
-R --no-save < update_IOTC_deps.R
