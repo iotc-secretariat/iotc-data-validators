0c8815aca1f4fa657e05ed34ab4fe2af8d3763e2
flowerside <flowerside@gmail.com>
2024-02-12 23:59:51 +0400

    Changing image name(s)

--- build_docker_image.arm.sh
+++ build_docker_image.arm.sh
@@ -1,4 +1,4 @@
 docker build --build-arg BB_user=$BITBUCKET_USER \
 			 --build-arg BB_password=$BITBUCKET_PASSWORD \
-			 -t iotc/validator-ui-arm:0.1.0 \
+			 -t iotc/validators-ui-arm:0.1.0 \
 			 -f Dockerfile.arm .
