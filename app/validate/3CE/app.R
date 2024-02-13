aef4c798c34aeab5df62b26371ee8c235d9cfae0
flowerside <flowerside@gmail.com>
2024-02-13 13:00:46 +0400

    Updating reference paths

--- app/validate/3CE/app.R
+++ app/validate/3CE/app.R
@@ -1,4 +1,4 @@
-source("../../app_common.R")
+source("../app_common.R")
 
 shinyApp(ui     = function()                       { return(common_ui    ("Form 3-CE", "IOTCForm3CE")) },
          server = function(input, output, session) { return(common_server("Form 3-CE", "IOTCForm3CE", input, output, session)) })
