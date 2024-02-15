5cd6af2a3b51c262d35e9cf92668a667b15074db
flowerside <flowerside@gmail.com>
2024-02-15 10:27:25 +0400

    Fixing extractors...

--- app/extract/update/3CE/app.R
+++ app/extract/update/3CE/app.R
@@ -1,4 +1,4 @@
 source("../../app_common.R")
 
-shinyApp(ui     = function()                       { return(common_ui    ("Form 3-CE-update", "IOTCForm3CEUpdate")) },
-         server = function(input, output, session) { return(common_server("Form 3-CE-update", "IOTCForm3CEUpdate", do_convert_3CE, input, output, session)) })
+shinyApp(ui     = function()                       { return(common_ui    ("Form 3CE (update)", "IOTCForm3CEUpdate")) },
+         server = function(input, output, session) { return(common_server("Form 3CE (update)", "IOTCForm3CEUpdate", do_convert_3CE, input, output, session)) })
--- app/extract/update/4SF/app.R
+++ app/extract/update/4SF/app.R
@@ -1,4 +1,4 @@
 source("../../app_common.R")
 
-shinyApp(ui     = function()                       { return(common_ui    ("Form 4-SF-update", "IOTCForm4SFUpdate")) },
-         server = function(input, output, session) { return(common_server("Form 4-SF-update", "IOTCForm4SFUpdate", do_convert_4SF, input, output, session)) })
+shinyApp(ui     = function()                       { return(common_ui    ("Form 4SF (update)", "IOTCForm4SFUpdate")) },
+         server = function(input, output, session) { return(common_server("Form 4SF (update)", "IOTCForm4SFUpdate", do_convert_4SF, input, output, session)) })
